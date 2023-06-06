55baa8de35a780af5395c9ff62bd902b82596ab9
brandontzhang <zhangtbrandon2@gmail.com>
2023-06-05 22:31:24 -0400

    Start on navbar and installed tailwind properly

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import './App.css'
+import { Navbar } from './components/Navbar/index';
 
 function App() {
   return (
-      <div>
-        Init Project
+      <div className="bg-[#18244A] h-screen w-auto">
+        <Navbar />
       </div>
   )
 }
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import { GiHamburgerMenu } from 'react-icons/gi'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className="bg-inherit absolute w-screen">
+      <ul className='hidden md:flex float-right flex-wrap justify-evenly'>
+        <li className="text-4xl p-2 m-2 text-slate-300 hover:bg-[#6177AD] rounded-md">Home</li>
+        <li className="text-4xl p-2 m-2 text-slate-300 hover:bg-[#6177AD] rounded-md">Work</li>
+        <li className="text-4xl p-2 m-2 text-slate-300 hover:bg-[#6177AD] rounded-md">Projects</li>
+        <li className="text-4xl p-2 m-2 text-slate-300 hover:bg-[#6177AD] rounded-md">Contact</li>
+      </ul>
+
+      <div className='md:hidden flex float-right'>
+        <GiHamburgerMenu className='text-5xl p-2 m-2 text-slate-300' />
+      </div>
+    </div>
   )
 }
 
