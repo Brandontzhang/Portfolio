414c1c668694eda4322dad0070a409b88bac32b4
brandontzhang <zhangtbrandon2@gmail.com>
2023-06-06 20:30:27 -0400

    Navbar display complete

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,19 +1,27 @@
-import React from 'react'
-import { GiHamburgerMenu } from 'react-icons/gi'
+import { useState } from 'react';
+import { GiHamburgerMenu } from 'react-icons/gi';
+import { AiOutlineClose } from 'react-icons/ai';
 
 const Navbar = () => {
+
+  const [expandMenu, setExpandMenu] = useState(false);
+
+  const tabs = ["Home", "Work", "Projects", "Contact"];
+
   return (
-    <div className="bg-inherit absolute w-screen">
+    <div className="bg-inherit absolute h-screen w-screen">
       <ul className='hidden md:flex float-right flex-wrap justify-evenly'>
-        <li className="text-4xl p-2 m-2 text-slate-300 hover:bg-[#6177AD] rounded-md">Home</li>
-        <li className="text-4xl p-2 m-2 text-slate-300 hover:bg-[#6177AD] rounded-md">Work</li>
-        <li className="text-4xl p-2 m-2 text-slate-300 hover:bg-[#6177AD] rounded-md">Projects</li>
-        <li className="text-4xl p-2 m-2 text-slate-300 hover:bg-[#6177AD] rounded-md">Contact</li>
+        {tabs.map(tab => <li className="text-4xl p-2 m-2 text-slate-300 hover:bg-[#6177AD] hover:cursor-pointer rounded-md">{tab}</li>)}
       </ul>
 
-      <div className='md:hidden flex float-right'>
-        <GiHamburgerMenu className='text-5xl p-2 m-2 text-slate-300' />
+      <div className='md:hidden flex float-right hover:cursor-pointer' onClick={() => {console.log("hi");setExpandMenu(expanded => !expanded)}}>
+        <GiHamburgerMenu className={!expandMenu ? 'text-5xl p-2 m-2 text-slate-300 z-10 rounded-lg' : 'hidden'} />
+        <AiOutlineClose className={expandMenu ? 'text-5xl p-2 m-2 text-slate-300 z-10 rounded-lg' : 'hidden'} />
       </div>
+
+      <ul className={expandMenu ? 'md:hidden absolute flex w-full h-full flex-col justify-center items-center' : 'hidden'}>
+        {tabs.map(tab => <li className='text-slate-300 py-10 text-5xl'>{tab}</li>)}
+      </ul>
     </div>
   )
 }
