b09a48859e4cf5500722a0f841a86363185481df
brandontzhang <zhangtbrandon2@gmail.com>
2023-06-15 20:10:46 -0400

    removed unnecessary useState

--- src/components/Skills/Bubble.tsx
+++ src/components/Skills/Bubble.tsx
@@ -1,7 +1,6 @@
 import { useState, useEffect } from 'react';
 
 const Bubble = (props : any) => {
-    const [img, setImg] = useState(props.img);
     const [padding, setPadding] = useState('p-4');
 
     useEffect(() => {
@@ -11,7 +10,7 @@ const Bubble = (props : any) => {
 
     return (
         <div className="bg-[#3479b1] w-20 h-20 rounded-full transition ease-in-out bg-blue hover:-translate-y-2 hover:scale-125 hover:bg-indigo-500 duration-300">
-            <img className={padding + ' object-contain'} src={img} />
+            <img className={padding + ' object-contain'} src={props.img} />
         </div>
     )
 }
