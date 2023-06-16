9d640d5a4988269314dd640d03ea72deb5f07a98
brandontzhang <zhangtbrandon2@gmail.com>
2023-06-15 20:58:25 -0400

    Added project card title

--- src/components/Projects/ProjectCard.tsx
+++ src/components/Projects/ProjectCard.tsx
@@ -7,19 +7,22 @@ const ProjectCard = (props : any) => {
 
 
     return (
-        <div className="bg-future bg-contain w-fit h-fit backdrop-blur-md rounded-lg m-16 -z-5">
-            <div className="h-[250px] w-[250px] rounded-lg transition ease-in-out hover:backdrop-blur-sm duration-1000" onMouseEnter={() => setHover(true)} onMouseLeave={() => setHover(false)}>
-                <div className={hover ? 'flex h-full w-full justify-evenly items-center' : 'hidden'}>
-
-                    <a href={codeUrl}>
-                        <button className=''>Code</button>
-                    </a>
-                    <a href={demoUrl}>
-                        <button>Demo</button>
-                    </a>
+        <div className=' m-16 -z-5'>
+            <div className='text-slate-300 mb-2'>{title}</div>
+            <div className="bg-future bg-contain w-fit h-fit backdrop-blur-md rounded-lg">
+                <div className="h-[250px] w-[250px] rounded-lg transition ease-in-out hover:backdrop-blur-sm duration-1000" onMouseEnter={() => setHover(true)} onMouseLeave={() => setHover(false)}>
+                    <div className={hover ? 'flex h-full w-full justify-evenly items-center' : 'hidden'}>
+                        <a href={codeUrl}>
+                            <button className=''>Code</button>
+                        </a>
+                        <a href={demoUrl}>
+                            <button>Demo</button>
+                        </a>
+                    </div>
                 </div>
             </div>
         </div>
+        
     )
 }
 
