c8f0af608d9bfae1a2d8a6a9fba7bd530ca9d7e4
brandontzhang <zhangtbrandon2@gmail.com>
2023-06-19 19:26:28 -0400

    Added scrolling

--- src/App.tsx
+++ src/App.tsx
@@ -6,15 +6,27 @@ import { Skills } from './components/Skills/index';
 import { Work } from './components/Work/index';
 import { Contact } from './components/Contact/index';
 
+import { Element, Link } from 'react-scroll';
+
 function App() {
   return (
       <div className='flex-col h-auto w-auto bg-[#18244A]'>
         <Navbar />
-        <Hero />
-        <Skills />
-        <Work />
-        <Projects />
-        <Contact />
+        <Element name='About Me'>
+          <Hero />
+        </Element>
+        <Element name='Skills'>
+          <Skills />
+        </Element>
+        <Element name='Work'>
+          <Work />
+        </Element>
+        <Element name='Projects'>
+          <Projects />
+        </Element>
+        <Element name='Contact'>
+          <Contact />
+        </Element>
       </div>
   )
 }
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,17 +1,18 @@
 import { useState } from 'react';
 import { GiHamburgerMenu } from 'react-icons/gi';
 import { AiOutlineClose } from 'react-icons/ai';
+import { Link } from 'react-scroll';
 
 const Navbar = () => {
 
   const [expandMenu, setExpandMenu] = useState(false);
 
-  const tabs = ["Home", "Work", "Projects", "Contact"];
+  const tabs = ["About Me", "Work", "Projects", "Contact"];
 
   return (
     <div className="bg-[#18244A] fixed h-18 w-full">
       <ul className='hidden md:flex float-right flex-wrap justify-evenly'>
-        {tabs.map((tab, index) => <li key={index} className="text-4xl py-1 px-4 m-2 text-slate-300 hover:bg-[#6177AD] hover:cursor-pointer rounded-md">{tab}</li>)}
+        {tabs.map((tab, index) => <li key={index} className="text-4xl py-1 px-4 m-2 text-slate-300 hover:bg-[#6177AD] hover:cursor-pointer rounded-md"><Link to={tab} smooth={true} offset={tab !== 'About Me' ? -75 : 0}>{tab}</Link></li>)}
       </ul>
 
       <div className='md:hidden flex float-right hover:cursor-pointer' onClick={() => setExpandMenu(expanded => !expanded)}>
@@ -20,7 +21,7 @@ const Navbar = () => {
       </div>
 
       <ul className={expandMenu ? 'md:hidden absolute bg-inherit flex w-full h-screen flex-col justify-center items-center' : 'hidden'}>
-        {tabs.map((tab, index) => <li key={index} className='text-slate-300 py-10 text-5xl'>{tab}</li>)}
+        {tabs.map((tab, index) => <li key={index} className='text-slate-300 py-10 text-5xl'><Link onClick={() => setExpandMenu(expanded => !expanded)} offset={tab !== 'About Me' ? -75 : 0} to={tab} smooth={true}>{tab}</Link></li>)}
       </ul>
     </div>
   )
